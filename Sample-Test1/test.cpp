e59ff3dcb98f46617c0df2b814b689f03bd068e8
hwoong <lhw3110@naver.com>
2023-07-06 11:10:22 +0900

    length tc develop

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,26 @@
 #include "pch.h"
+#include "../SimilarityCheck/source.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(LengthCheck, TC1) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckLength("ASD", "DSA"), 60);
+}
+
+TEST(LengthCheck, TC2) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckLength("A", "BB"), 0);
+}
+
+TEST(LengthCheck, TC3) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckLength("AAABB", "BAA"), 60);
+}
+
+TEST(LengthCheck, TC4) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckLength("AA", "AAE"), 60);
 }
---
+++ SimilarityCheck/source.cpp
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class SimilarityChecker
+{
+public:
+	int CheckLength(string s1, string s2)
+	{
+		
+	}
+private:
+};
