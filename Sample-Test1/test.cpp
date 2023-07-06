d338b57077774b1f02d36c1194e56d25a2e7fbc1
hwoong <lhw3110@naver.com>
2023-07-06 11:19:22 +0900

    alpha tc develop

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,26 @@
 #include "pch.h"
+#include "../SimilarityCheck/source.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(AlphaCheck, TC1) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckAlpha("ASD", "DSA"), 40);
+}
+
+TEST(AlphaCheck, TC2) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckAlpha("A", "BB"), 0);
+}
+
+TEST(AlphaCheck, TC3) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckAlpha("AAABB", "BA"), 40);
+}
+
+TEST(AlphaCheck, TC4) {
+	SimilarityChecker sc;
+
+	EXPECT_EQ(sc.CheckAlpha("AA", "AAE"), 20);
 }
---
+++ SimilarityCheck/source.cpp
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class SimilarityChecker
+{
+public:
+	int CheckAlpha(string s1, string s2)
+	{
+		
+	}
+private:
+};
